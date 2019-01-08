d5f2203ecdbab5e59043b78394327a1e3921633c
Mark Bell <me@markb.co.uk>
2019-01-08 07:46:26 +0000

    Experimenting with Harmony

--- CodeInjectionSpike/Controllers/HomeController.cs
+++ CodeInjectionSpike/Controllers/HomeController.cs
@@ -1,22 +1,35 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Harmony;
 
 namespace CodeInjectionSpike.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return Content("TEST");
-        }
+        private readonly HarmonyInstance _harmony = HarmonyInstance.Create("CodeInjectionSpike");
+
+        public ActionResult Index() => Content("TEST");
 
         public ActionResult TestMode()
         {
+            var original = GetType().GetMethod("Index");
+            var prefix = GetType().GetMethod("ThrowException");
+
+            _harmony.Patch(original, new HarmonyMethod(prefix));
+
             return Content("IN TEST MODE");
         }
 
         public ActionResult Reset()
         {
+            _harmony.UnpatchAll("CodeInjectionSpike");
+
             return Content("METHOD RESET");
         }
+
+        // NOTE: Pre/postfix methods must be static; otherwise, rewriting
+        // their parent class will generate an invalid program
+        public static void ThrowException() =>
+            throw new Exception("TEST EXCEPTION");
     }
 }
